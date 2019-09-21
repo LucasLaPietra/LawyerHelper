6c002f103264e568723da5806b7aa87d046eb914
Fran Alba <fralba98@gmail.com>
2019-09-20 22:49:19 -0300

    agregado bajaCobro, bajaRecordatorio, consultaCobro

---
+++ WindowsFormsApp2/UI/Cobros/ConsultaCobro.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace LawyerHelper.UI.Cobros
+{
+    partial class ConsultaCobro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BotonCancelar = new System.Windows.Forms.Button();
+            this.LabelAM = new System.Windows.Forms.Label();
+            this.LabelMinutos = new System.Windows.Forms.Label();
+            this.LabelHora2 = new System.Windows.Forms.Label();
+            this.LabelDetalle2 = new System.Windows.Forms.Label();
+            this.LabelImporte2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LabelDetalle = new System.Windows.Forms.Label();
+            this.LabelImporte = new System.Windows.Forms.Label();
+            this.LabelHora = new System.Windows.Forms.Label();
+            this.checkedListBoxCobros = new System.Windows.Forms.CheckedListBox();
+            this.LabelFecha = new System.Windows.Forms.Label();
+            this.dateTimePickerFecha = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // BotonCancelar
+            // 
+            this.BotonCancelar.Location = new System.Drawing.Point(263, 460);
+            this.BotonCancelar.Name = "BotonCancelar";
+            this.BotonCancelar.Size = new System.Drawing.Size(97, 24);
+            this.BotonCancelar.TabIndex = 151;
+            this.BotonCancelar.Text = "Cancelar";
+            this.BotonCancelar.UseVisualStyleBackColor = true;
+            // 
+            // LabelAM
+            // 
+            this.LabelAM.AutoSize = true;
+            this.LabelAM.Location = new System.Drawing.Point(117, 247);
+            this.LabelAM.Name = "LabelAM";
+            this.LabelAM.Size = new System.Drawing.Size(10, 13);
+            this.LabelAM.TabIndex = 150;
+            this.LabelAM.Text = "-";
+            // 
+            // LabelMinutos
+            // 
+            this.LabelMinutos.AutoSize = true;
+            this.LabelMinutos.Location = new System.Drawing.Point(101, 247);
+            this.LabelMinutos.Name = "LabelMinutos";
+            this.LabelMinutos.Size = new System.Drawing.Size(10, 13);
+            this.LabelMinutos.TabIndex = 149;
+            this.LabelMinutos.Text = "-";
+            // 
+            // LabelHora2
+            // 
+            this.LabelHora2.AutoSize = true;
+            this.LabelHora2.Location = new System.Drawing.Point(67, 247);
+            this.LabelHora2.Name = "LabelHora2";
+            this.LabelHora2.Size = new System.Drawing.Size(10, 13);
+            this.LabelHora2.TabIndex = 148;
+            this.LabelHora2.Text = "-";
+            // 
+            // LabelDetalle2
+            // 
+            this.LabelDetalle2.AutoSize = true;
+            this.LabelDetalle2.Location = new System.Drawing.Point(68, 319);
+            this.LabelDetalle2.Name = "LabelDetalle2";
+            this.LabelDetalle2.Size = new System.Drawing.Size(10, 13);
+            this.LabelDetalle2.TabIndex = 147;
+            this.LabelDetalle2.Text = "-";
+            // 
+            // LabelImporte2
+            // 
+            this.LabelImporte2.AutoSize = true;
+            this.LabelImporte2.Location = new System.Drawing.Point(67, 282);
+            this.LabelImporte2.Name = "LabelImporte2";
+            this.LabelImporte2.Size = new System.Drawing.Size(10, 13);
+            this.LabelImporte2.TabIndex = 146;
+            this.LabelImporte2.Text = "-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 247);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 13);
+            this.label3.TabIndex = 145;
+            this.label3.Text = ":";
+            // 
+            // LabelDetalle
+            // 
+            this.LabelDetalle.AutoSize = true;
+            this.LabelDetalle.Location = new System.Drawing.Point(10, 319);
+            this.LabelDetalle.Name = "LabelDetalle";
+            this.LabelDetalle.Size = new System.Drawing.Size(40, 13);
+            this.LabelDetalle.TabIndex = 144;
+            this.LabelDetalle.Text = "Detalle";
+            // 
+            // LabelImporte
+            // 
+            this.LabelImporte.AutoSize = true;
+            this.LabelImporte.Location = new System.Drawing.Point(10, 282);
+            this.LabelImporte.Name = "LabelImporte";
+            this.LabelImporte.Size = new System.Drawing.Size(42, 13);
+            this.LabelImporte.TabIndex = 143;
+            this.LabelImporte.Text = "Importe";
+            // 
+            // LabelHora
+            // 
+            this.LabelHora.AutoSize = true;
+            this.LabelHora.Location = new System.Drawing.Point(10, 247);
+            this.LabelHora.Name = "LabelHora";
+            this.LabelHora.Size = new System.Drawing.Size(30, 13);
+            this.LabelHora.TabIndex = 142;
+            this.LabelHora.Text = "Hora";
+            // 
+            // checkedListBoxCobros
+            // 
+            this.checkedListBoxCobros.FormattingEnabled = true;
+            this.checkedListBoxCobros.Location = new System.Drawing.Point(12, 52);
+            this.checkedListBoxCobros.Name = "checkedListBoxCobros";
+            this.checkedListBoxCobros.Size = new System.Drawing.Size(348, 169);
+            this.checkedListBoxCobros.TabIndex = 141;
+            // 
+            // LabelFecha
+            // 
+            this.LabelFecha.AutoSize = true;
+            this.LabelFecha.Location = new System.Drawing.Point(9, 19);
+            this.LabelFecha.Name = "LabelFecha";
+            this.LabelFecha.Size = new System.Drawing.Size(40, 13);
+            this.LabelFecha.TabIndex = 140;
+            this.LabelFecha.Text = "Fecha:";
+            // 
+            // dateTimePickerFecha
+            // 
+            this.dateTimePickerFecha.Location = new System.Drawing.Point(55, 13);
+            this.dateTimePickerFecha.Name = "dateTimePickerFecha";
+            this.dateTimePickerFecha.Size = new System.Drawing.Size(193, 20);
+            this.dateTimePickerFecha.TabIndex = 139;
+            // 
+            // ConsultaCobro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 496);
+            this.Controls.Add(this.BotonCancelar);
+            this.Controls.Add(this.LabelAM);
+            this.Controls.Add(this.LabelMinutos);
+            this.Controls.Add(this.LabelHora2);
+            this.Controls.Add(this.LabelDetalle2);
+            this.Controls.Add(this.LabelImporte2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LabelDetalle);
+            this.Controls.Add(this.LabelImporte);
+            this.Controls.Add(this.LabelHora);
+            this.Controls.Add(this.checkedListBoxCobros);
+            this.Controls.Add(this.LabelFecha);
+            this.Controls.Add(this.dateTimePickerFecha);
+            this.Name = "ConsultaCobro";
+            this.Text = "ConsultaCobro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BotonCancelar;
+        private System.Windows.Forms.Label LabelAM;
+        private System.Windows.Forms.Label LabelMinutos;
+        private System.Windows.Forms.Label LabelHora2;
+        private System.Windows.Forms.Label LabelDetalle2;
+        private System.Windows.Forms.Label LabelImporte2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label LabelDetalle;
+        private System.Windows.Forms.Label LabelImporte;
+        private System.Windows.Forms.Label LabelHora;
+        private System.Windows.Forms.CheckedListBox checkedListBoxCobros;
+        private System.Windows.Forms.Label LabelFecha;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFecha;
+    }
+}
