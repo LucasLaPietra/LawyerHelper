3a2a2f9bc796645e64ba45ff8c475e30a86ee769
lucas la pietra <lux_lp@hotmail.com>
2019-10-10 16:58:26 -0300

    Carpetas nuevas

--- WindowsFormsApp2/Controladores/Controlador.cs
+++ WindowsFormsApp2/Controladores/Controlador.cs
@@ -89,5 +89,53 @@ namespace LawyerHelper.Clases
             }
             return iQuery;
         }
+
+        public IList<Persona> BusquedaJuicios(string Parametro, string Valor)
+        {
+            Contexto iContexto = new Contexto();
+            List<Persona> iQuery;
+            //No se puede utilizar variables como atributos, ya que estos deben estar definidos en tiempo de compilacion
+            //Por eso utilice el switch para cada atributo en particular
+            switch (Parametro)
+            {
+                case "Nombre":
+                    iQuery = iContexto.Personas.Where(n => n.Nombre == Valor).ToList();
+                    break;
+                case "Apellido":
+                    iQuery = iContexto.Personas.Where(n => n.Apellido == Valor).ToList();
+                    break;
+                case "Dni":
+                    iQuery = iContexto.Personas.Where(n => n.Dni == Valor).ToList();
+                    break;
+                case "Domicilio":
+                    iQuery = iContexto.Personas.Where(n => n.Domicilio == Valor).ToList();
+                    break;
+                case "Telefono":
+                    iQuery = iContexto.Personas.Where(n => n.Telefono == Valor).ToList();
+                    break;
+                case "Profesion":
+                    iQuery = iContexto.Personas.Where(n => n.Profesion == Valor).ToList();
+                    break;
+                case "LugarTrabajo":
+                    iQuery = iContexto.Personas.Where(n => n.LugarTrabajo == Valor).ToList();
+                    break;
+                case "Cuil":
+                    iQuery = iContexto.Personas.Where(n => n.Cuil == Valor).ToList();
+                    break;
+                case "EstadoCivil":
+                    iQuery = iContexto.Personas.Where(n => n.EstadoCivil == Valor).ToList();
+                    break;
+                case "Representante":
+                    iQuery = iContexto.Personas.Where(n => n.Representante == Valor).ToList();
+                    break;
+                case "DomicilioLegal":
+                    iQuery = iContexto.Personas.Where(n => n.DomicilioLegal == Valor).ToList();
+                    break;
+                default:
+                    iQuery = null;
+                    break;
+            }
+            return iQuery;
+        }
     }
 }
