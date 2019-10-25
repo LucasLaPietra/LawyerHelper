d6717534a24b389ab398d92764d6e2121fa1a4b1
lauretamar <lautaroretamar@gmail.com>
2019-10-25 10:54:29 -0300

    BorradosdelaClase RepositorioGeneralcomentariosxml

--- WindowsFormsApp2/DAL/Repositorio/RepositorioGeneral.cs
+++ WindowsFormsApp2/DAL/Repositorio/RepositorioGeneral.cs
@@ -21,18 +21,12 @@ namespace LawyerHelper.DAL.Repositorio
             }
             this.iContext = pContext;
         }
-        /// <summary>
-        /// Agrego un objeto al repositorio
-        /// </summary>
-        /// <param name="pEntity">objeto a agregar</param>
+        
         public void Agregar(TEntity pEntity)
         {
             this.iContext.Set<TEntity>().Add(pEntity);
         }
-        /// <summary>
-        /// Elimina un objeto del repositorio
-        /// </summary>
-        /// <param name="pEntity">objeto a eliminar</param>
+        
         public void Eliminar(TEntity pEntity)
         {
             if (pEntity == null)
@@ -41,10 +35,7 @@ namespace LawyerHelper.DAL.Repositorio
             }
             this.iContext.Set<TEntity>().Remove(pEntity);
         }
-        /// <summary>
-        /// Modifica un objeto del repositorio
-        /// </summary>
-        /// <param name="pEntity">objeto a modificar</param>
+       
         public void Modificar(TEntity pEntity)
         {
             if (pEntity == null)
@@ -53,19 +44,12 @@ namespace LawyerHelper.DAL.Repositorio
             }
             this.iContext.Entry(pEntity).State = EntityState.Modified;
         }
-        /// <summary>
-        /// Obtiene un objeto del repositorio a traves del id
-        /// </summary>
-        /// <param name="pId">identificador del objeto a buscar</param>
-        /// <returns></returns>
+        
         public TEntity Obtener(int pId)
         {
             return this.iContext.Set<TEntity>().Find(pId);
         }
-        /// <summary>
-        /// Genera una lista de todos objetos del repositorio
-        /// </summary>
-        /// <returns></returns>
+        
         public IList<TEntity> ObtenerTodos()
         {
             return this.iContext.Set<TEntity>().ToList();
